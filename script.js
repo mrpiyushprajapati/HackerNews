150ad41f909dc625693d849673a7131c8e1ec3b3
Shubham Goel <sshubhamgoel24@gmail.com>
2021-10-14 17:31:41 +0530

    Ui refined

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ $.get("https://hacker-news.firebaseio.com/v0/topstories.json",function(listdata)
     $.each(listdata,function(i,item){
         x++;
         $.get("https://hacker-news.firebaseio.com/v0/item/"+item+".json",function(news){
-            $('#container').append($('<div class="item col-sm-11 col-md-5"><a href="' +news.url+ '"><p class="title">' +news.title+ '</p><p class="by">By- ' +news.by+ '</p><p class="time"> Created On: ' +Date(news.time*1000)+ '</p></a></div>'));
+            const d = new Date(news.time*1000)
+            $('#container').append($('<div class="item col-sm-11 col-md-5"><a href="' +news.url+ '"><p class="title">' +news.title+ '</p><p class="by">By- ' +news.by+ '<br> Created On: ' +d.toDateString()+ ' '+ d.getHours() + ':' + d.getMinutes() + '</p></a></div>'));
         });
         if(x>=50){
             return false;
