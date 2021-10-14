83b262ebc73f659d67e647763b657a98a0eea460
Shubham Goel <sshubhamgoel24@gmail.com>
2021-10-14 16:41:43 +0530

    Show latest 50 news

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-var i=500;
-$.get("https://hacker-news.firebaseio.com/v0/showstories.json",function(listdata){
-    
-    console.log(listdata);
-    $.each(Object.keys(listdata),function(i,item){
+var x = 0;
+$.get("https://hacker-news.firebaseio.com/v0/topstories.json",function(listdata){
+    $.each(listdata,function(i,item){
+        x++;
         $.get("https://hacker-news.firebaseio.com/v0/item/"+item+".json",function(news){
-            if(news.type == 'story'){
-                console.log(news);
-                $('#container').append($('<div class="item col-sm-11 col-md-5"><a href="' +news.url+ '"><p class="title">' +news.title+ '</p><p class="by">' +news.by+ '</p><p class="time">' +Date(news.time*1000)+ '</p></a></div>'));
-            }
+            $('#container').append($('<div class="item col-sm-11 col-md-5"><a href="' +news.url+ '"><p class="title">' +news.title+ '</p><p class="by">By- ' +news.by+ '</p><p class="time"> Created On: ' +Date(news.time*1000)+ '</p></a></div>'));
         });
+        if(x>=50){
+            return false;
+        }
     });   
 });
